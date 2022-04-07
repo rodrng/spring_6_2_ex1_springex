eb730af9cd4d5fab6858674d6c7e9f571ea503b5
경민 <rodrng@gmail.com>
2022-04-07 20:49:55 +0900

    DI 설정방법 (JAVA를 이용한 DI)

---
+++ src/main/java/com/javagyeongmin/ex/MainClass.java
@@ -0,0 +1,25 @@
+package com.javagyeongmin.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		//컨테이너 객체를 ApplicationConfig 클래스로 설정하여 생성
+		
+		Student student1 = ctx.getBean("student1",Student.class);
+		Student student2 = ctx.getBean("student2",Student.class);
+		
+		System.out.println(student1.getName());//홍길동
+		System.out.println(student2.getName());//이순신
+		
+		ctx.close();
+		
+		
+		
+	}
+
+}
